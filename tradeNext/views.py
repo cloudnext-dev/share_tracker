2cd398dd627ec7edd6aa0b6b743c561e7fd659b8
Ankit Rustagi <ankit.rustagi@hcl.com>
2021-07-30 18:39:36 +0530

    Updated-Maintenance Mode

--- tradeNext/views.py
+++ tradeNext/views.py
@@ -13,7 +13,7 @@ from datetime import datetime
 
 def Index(request):
 	set_maintenance_mode(False)
-    return render(request, 'index.html')
+	return render(request, 'index.html')
 
 def limitTrade(request):
 	set_maintenance_mode(False)
